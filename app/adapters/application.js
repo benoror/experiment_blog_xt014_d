c514f8d93ced73bddc2b47bad5608facc35ef29e
Ben Orozco <benoror@gmail.com>
2016-12-06 16:38:48 -0600

    chore: update to gomix.me domain

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,7 @@
 import AirtableAdapter from "ember-airtable/adapter";
 
 export default AirtableAdapter.extend({
-  host: 'https://keen-gorilla.hyperdev.space',
+  host: 'https://keen-gorilla.gomix.me',
   //host: 'https://proxyblogxt014d.now.sh',
   //host: 'http://localhost:4214',
   namespace: 'api'
